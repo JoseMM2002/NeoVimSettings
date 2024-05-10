57db05172906045376a07928c19c2cc54bbcf744
JoseMM2002 <jeolmmt@gmail.com>
2024-05-10 01:15:29 -0600

    colorizer plugin

---
+++ lua/josemm/lazy/colorizer.lua
@@ -0,0 +1,19 @@
+return {
+	"norcalli/nvim-colorizer.lua",
+	config = function()
+		require("colorizer").setup({
+			css = {
+				css = true,
+				css_fn = true,
+			},
+			scss = {
+				css = true,
+				css_fn = true,
+			},
+			sass = {
+				css = true,
+				css_fn = true,
+			},
+		})
+	end,
+}
