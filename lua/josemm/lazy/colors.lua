3e2e7f45856c23f069232c3414b94a1a95a7ab8f
JoseMM2002 <jeolmmt@gmail.com>
2024-10-24 13:03:02 -0600

    another color

--- lua/josemm/lazy/colors.lua
+++ lua/josemm/lazy/colors.lua
@@ -35,4 +35,17 @@ return {
 			})
 		end,
 	},
+	-- Lua
+
+	{
+		"olivercederborg/poimandres.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("poimandres").setup({})
+		end,
+		init = function()
+			-- vim.cmd("colorscheme poimandres")
+		end,
+	},
 }
