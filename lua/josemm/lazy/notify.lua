0c41f3ee2d569cd587b0bfee10bde643eae8889f
JoseMM2002 <jeolmmt@gmail.com>
2024-12-13 09:24:43 -0600

    remove notification

--- lua/josemm/lazy/notify.lua
+++ lua/josemm/lazy/notify.lua
@@ -4,9 +4,5 @@ return {
 	config = function()
 		local notify = require("notify")
 		vim.notify = notify
-		vim.notify("Notify loaded", "info", {
-			title = "Notify",
-			timeout = 1000,
-		})
 	end,
 }
