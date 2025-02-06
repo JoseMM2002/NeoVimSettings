8e2658acab54f2529771e367afb22b2c64fade97
JoseMM2002 <jeolmmt@gmail.com>
2025-02-06 02:12:03 -0600

    remove old formatter

--- lua/josemm/lazy/formatter.lua
+++ lua/josemm/lazy/formatter.lua
@@ -1,54 +1,4 @@
-function setFormatters()
-	local formatters = {
-		prettier = {
-			"javascript",
-			"typescript",
-			"vue",
-			"javascriptreact",
-			"typescriptreact",
-			"html",
-			"css",
-			"json",
-		},
-		stylua = {
-			"lua",
-		},
-		taplo = {
-			"toml",
-		},
-		shfmt = {
-			"sh",
-		},
-		sql_formatter = {
-			"sql",
-		},
-	}
-
-	local formatedConfig = {}
-
-	for formatter, filetypes in pairs(formatters) do
-		for _, filetype in ipairs(filetypes) do
-			if not formatedConfig[filetype] then
-				formatedConfig[filetype] = {}
-			end
-			table.insert(formatedConfig[filetype], require("formatter.filetypes." .. filetype)[formatter])
-		end
-	end
-	return formatedConfig
-end
-
 return {
-	-- "mhartington/formatter.nvim",
-	-- config = function()
-	-- 	require("formatter").setup({
-	-- 		filetype = setFormatters(),
-	-- 	})
-
-	-- 	vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-	-- 		command = "FormatWriteLock",
-	-- 	})
-	-- end,
-	--
 	"stevearc/conform.nvim",
 	config = function()
 		local conform = require("conform")
@@ -66,7 +16,6 @@ return {
 				scss = { "prettier" },
 				markdown = { "prettier" },
 				sass = { "prettier" },
-
 				lua = { "stylua" },
 				toml = { "taplo" },
 				sh = { "shfmt" },
