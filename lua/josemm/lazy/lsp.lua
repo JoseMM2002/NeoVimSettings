470626baacbfd960169441fa7b74b96abff426ae
JoseMM2002 <jeolmmt@gmail.com>
2025-03-21 10:31:28 -0600

    update lsp cmp with blink cmp,colorful menu and hovercraft

--- lua/josemm/lazy/lsp.lua
+++ lua/josemm/lazy/lsp.lua
@@ -11,19 +11,187 @@ return {
 			},
 		},
 	},
-	{ "hrsh7th/cmp-nvim-lsp" },
-	{ "hrsh7th/nvim-cmp" },
+	{ "echasnovski/mini.icons" },
+	{
+		"xzbdmw/colorful-menu.nvim",
+		config = function()
+			-- You don't need to set these options.
+			require("colorful-menu").setup({
+				ls = {
+					lua_ls = {
+						-- Maybe you want to dim arguments a bit.
+						arguments_hl = "@comment",
+					},
+					gopls = {
+						-- By default, we render variable/function's type in the right most side,
+						-- to make them not to crowd together with the original label.
+
+						-- when true:
+						-- foo             *Foo
+						-- ast         "go/ast"
+
+						-- when false:
+						-- foo *Foo
+						-- ast "go/ast"
+						align_type_to_right = true,
+						-- When true, label for field and variable will format like "foo: Foo"
+						-- instead of go's original syntax "foo Foo". If align_type_to_right is
+						-- true, this option has no effect.
+						add_colon_before_type = false,
+						-- See https://github.com/xzbdmw/colorful-menu.nvim/pull/36
+						preserve_type_when_truncate = true,
+					},
+					-- for lsp_config or typescript-tools
+					ts_ls = {
+						-- false means do not include any extra info,
+						-- see https://github.com/xzbdmw/colorful-menu.nvim/issues/42
+						extra_info_hl = "@comment",
+					},
+					vtsls = {
+						-- false means do not include any extra info,
+						-- see https://github.com/xzbdmw/colorful-menu.nvim/issues/42
+						extra_info_hl = "@comment",
+					},
+					["rust-analyzer"] = {
+						-- Such as (as Iterator), (use std::io).
+						extra_info_hl = "@comment",
+						-- Similar to the same setting of gopls.
+						align_type_to_right = true,
+						-- See https://github.com/xzbdmw/colorful-menu.nvim/pull/36
+						preserve_type_when_truncate = true,
+					},
+					clangd = {
+						-- Such as "From <stdio.h>".
+						extra_info_hl = "@comment",
+						-- Similar to the same setting of gopls.
+						align_type_to_right = true,
+						-- the hl group of leading dot of "•std::filesystem::permissions(..)"
+						import_dot_hl = "@comment",
+						-- See https://github.com/xzbdmw/colorful-menu.nvim/pull/36
+						preserve_type_when_truncate = true,
+					},
+					zls = {
+						-- Similar to the same setting of gopls.
+						align_type_to_right = true,
+					},
+					roslyn = {
+						extra_info_hl = "@comment",
+					},
+					dartls = {
+						extra_info_hl = "@comment",
+					},
+					-- The same applies to pyright/pylance
+					basedpyright = {
+						-- It is usually import path such as "os"
+						extra_info_hl = "@comment",
+					},
+					-- If true, try to highlight "not supported" languages.
+					fallback = true,
+					-- this will be applied to label description for unsupport languages
+					fallback_extra_info_hl = "@comment",
+				},
+				-- If the built-in logic fails to find a suitable highlight group for a label,
+				-- this highlight is applied to the label.
+				fallback_highlight = "@variable",
+				-- If provided, the plugin truncates the final displayed text to
+				-- this width (measured in display cells). Any highlights that extend
+				-- beyond the truncation point are ignored. When set to a float
+				-- between 0 and 1, it'll be treated as percentage of the width of
+				-- the window: math.floor(max_width * vim.api.nvim_win_get_width(0))
+				-- Default 60.
+				max_width = 60,
+			})
+		end,
+	},
+	{
+		"saghen/blink.cmp",
+		version = "*",
+		opts = {
+			keymap = {
+				preset = "default",
+				["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+				["<C-e>"] = { "hide", "fallback" },
+				["<CR>"] = { "accept", "fallback" },
+
+				["<Tab>"] = { "snippet_forward", "fallback" },
+				["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+				["<Up>"] = { "select_prev", "fallback" },
+				["<Down>"] = { "select_next", "fallback" },
+				["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+				["<C-n>"] = { "select_next", "fallback_to_mappings" },
+
+				["<C-b>"] = { "scroll_documentation_up", "fallback" },
+				["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+				["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+			},
+			sources = {
+				default = { "ecolog", "lsp", "path", "snippets", "buffer" },
+				providers = {
+					ecolog = { name = "ecolog", module = "ecolog.integrations.cmp.blink_cmp" },
+					buffer = {
+						opts = {
+							-- or (recommended) filter to only "normal" buffers
+							get_bufnrs = function()
+								return vim.tbl_filter(function(bufnr)
+									return vim.bo[bufnr].buftype == ""
+								end, vim.api.nvim_list_bufs())
+							end,
+						},
+					},
+				},
+			},
+			completion = {
+				menu = {
+					border = "rounded",
+					draw = {
+						components = {
+							kind_icon = {
+								ellipsis = false,
+								text = function(ctx)
+									local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+									return kind_icon
+								end,
+								-- Optionally, you may also use the highlights from mini.icons
+								--
+								highlight = function(ctx)
+									local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+									return hl
+								end,
+							},
+							label = {
+								text = function(ctx)
+									return require("colorful-menu").blink_components_text(ctx)
+								end,
+								highlight = function(ctx)
+									return require("colorful-menu").blink_components_highlight(ctx)
+								end,
+							},
+						},
+						columns = { { "kind_icon", "kind", gap = 2 }, { "label", "label_description", gap = 2 } },
+						treesitter = { "lsp" },
+						nil,
+					},
+				},
+				documentation = { auto_show = true, auto_show_delay_ms = 200 },
+				ghost_text = { enabled = true },
+				keyword = {
+					range = "full",
+				},
+				list = { selection = { preselect = false } },
+			},
+			signature = { window = { border = "rounded" }, enabled = true },
+			fuzzy = { implementation = "prefer_rust_with_warning" },
+		},
+		opts_extend = { "sources.default" },
+	},
 	{ "L3MON4D3/LuaSnip" },
-	{ "hrsh7th/cmp-buffer" },
-	{ "hrsh7th/cmp-nvim-lua" },
-	{ "onsails/lspkind.nvim" },
-	{ "rafamadriz/friendly-snippets" },
-	{ "saadparwaiz1/cmp_luasnip" },
 	{
 		"philosofonusus/ecolog.nvim",
 		opts = {
 			integrations = {
-				nvim_cmp = true,
+				blink_cmp = true,
 			},
 			shelter = {
 				configuration = {
@@ -48,6 +216,8 @@ return {
 		branch = "v3.x",
 		config = function()
 			local lsp_zero = require("lsp-zero")
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
+
 			lsp_zero.extend_lspconfig()
 			lsp_zero.on_attach(function(client, bufnr)
 				lsp_zero.default_keymaps({ buffer = bufnr })
@@ -88,10 +258,10 @@ return {
 				},
 				handlers = {
 					function(server_name)
-						require("lspconfig")[server_name].setup({})
+						require("lspconfig")[server_name].setup({ capabilities = capabilities })
 					end,
 					volar = function()
-						require("lspconfig").volar.setup({})
+						require("lspconfig").volar.setup({ capabilities = capabilities })
 					end,
 					ts_ls = function()
 						local vue_typescript_plugin = require("mason-registry")
@@ -117,44 +287,109 @@ return {
 								"typescript.tsx",
 								"vue",
 							},
+							capabilities = capabilities,
 						})
 					end,
 				},
 			})
 
-			local cmp = require("cmp")
-			local cmp_action = require("lsp-zero").cmp_action()
 			require("luasnip.loaders.from_vscode").lazy_load()
-
 			require("luasnip").filetype_extend("vue", { "vue" })
-
-			cmp.setup({
-				sources = {
-					{ name = "nvim_lsp" },
-					{ name = "nvim_lua" },
-					{ name = "ecolog" },
-					{ name = "luasnip" },
-					{ name = "buffer" },
-				},
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-f>"] = cmp_action.luasnip_jump_forward(),
-					["<C-b>"] = cmp_action.luasnip_jump_backward(),
-					["<CR>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
-				}),
-				formatting = {
-					fields = { "abbr", "kind", "menu" },
-					format = require("lspkind").cmp_format({
-						mode = "symbol", -- show only symbol annotations
-						maxwidth = 80, -- prevent the popup from showing more than provided characters
-						ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
-					}),
-				},
-			})
 		end,
+	},
+	{
+		"patrickpichler/hovercraft.nvim",
+
+		dependencies = {
+			{ "nvim-lua/plenary.nvim" },
+		},
+
+		-- this is the default config and can be skipped
+		opts = function()
+			return {
+				providers = {
+					providers = {
+						{
+							"LSP",
+							require("hovercraft.provider.lsp.hover").new(),
+						},
+						{
+							"Diagnostics",
+							require("hovercraft.provider.diagnostics").new(),
+						},
+						{
+							"Man",
+							require("hovercraft.provider.man").new(),
+						},
+						{
+							"Dictionary",
+							require("hovercraft.provider.dictionary").new(),
+						},
+						{
+							"Github Issue",
+							require("hovercraft.provider.github.issue").new(),
+						},
+						{
+							"Github Repo",
+							require("hovercraft.provider.github.repo").new(),
+						},
+						{
+							"Github User",
+							require("hovercraft.provider.github.user").new(),
+						},
+						{
+							"Github Blame",
+							require("hovercraft.provider.git.blame").new(),
+						},
+					},
+				},
+
+				window = {
+					border = "rounded",
+				},
+
+				keys = {
+					{
+						"<C-u>",
+						function()
+							require("hovercraft").scroll({ delta = -4 })
+						end,
+					},
+					{
+						"<C-d>",
+						function()
+							require("hovercraft").scroll({ delta = 4 })
+						end,
+					},
+					{
+						"<C-n>",
+						function()
+							require("hovercraft").hover_next()
+						end,
+					},
+					{
+						"<C-p>",
+						function()
+							require("hovercraft").hover_next({ step = -1 })
+						end,
+					},
+				},
+			}
+		end,
+
+		keys = {
+			{
+				"K",
+				function()
+					local hovercraft = require("hovercraft")
+
+					if hovercraft.is_visible() then
+						hovercraft.enter_popup()
+					else
+						hovercraft.hover()
+					end
+				end,
+			},
+		},
 	},
 }
