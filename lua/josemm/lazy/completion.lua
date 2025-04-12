41aa934e825ace9de143ec7a165b946458a7f988
JoseMM2002 <jeolmmt@gmail.com>
2025-04-11 18:55:49 -0600

    start cmdline only when more to 3 chars

--- lua/josemm/lazy/completion.lua
+++ lua/josemm/lazy/completion.lua
@@ -175,6 +175,15 @@ return {
 						module = "blink-cmp-avante",
 						name = "Avante",
 					},
+					cmdline = {
+						min_keyword_length = function(ctx)
+							-- when typing a command, only show when the keyword is 3 characters or longer
+							if ctx.mode == "cmdline" and string.find(ctx.line, " ") == nil then
+								return 3
+							end
+							return 0
+						end,
+					},
 				},
 			},
 			completion = {
