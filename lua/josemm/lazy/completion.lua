87936372084cdb1d3c56a63dec3df7c7ad45a90f
JoseMM2002 <jeolmmt@gmail.com>
2025-04-13 10:23:25 -0600

    add ctrly to accept completions

--- lua/josemm/lazy/completion.lua
+++ lua/josemm/lazy/completion.lua
@@ -127,6 +127,7 @@ return {
 				["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
 				["<C-e>"] = { "hide", "fallback" },
 				["<CR>"] = { "accept", "fallback" },
+				["<C-y>"] = { "accept", "fallback" },
 
 				["<Tab>"] = { "snippet_forward", "fallback" },
 				["<S-Tab>"] = { "snippet_backward", "fallback" },
