0635f14c4398d8ab2be83727ce8da7e45c810477
JoseMM2002 <Jose.Morales3@thomsonreuters.com>
2025-05-09 22:53:31 -0600

    recorder + updates

--- lua/josemm/lazy/lualine.lua
+++ lua/josemm/lazy/lualine.lua
@@ -1,33 +1,41 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	config = function()
-		require("lualine").setup({
-			options = {
-				component_separators = "",
-				section_separators = { left = "", right = "" },
-			},
-			sections = {
-				lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
-				lualine_b = { "filename", "branch" },
-				lualine_c = {
-					"%=", --[[ add your center components here in place of this comment ]]
+	{
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			require("lualine").setup({
+				options = {
+					component_separators = "",
+					section_separators = { left = "", right = "" },
 				},
-				lualine_x = {},
-				lualine_y = { "filetype", "progress" },
-				lualine_z = {
-					{ "location", separator = { right = "" }, left_padding = 2 },
+				sections = {
+					lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+					lualine_b = { "filename", "branch" },
+					lualine_c = {
+						"%=", --[[ add your center components here in place of this comment ]]
+					},
+					lualine_x = {},
+					lualine_y = { "filetype", "progress", { require("recorder").displaySlots } },
+					lualine_z = {
+						{ "location", separator = { right = "" }, left_padding = 2 },
+						{ require("recorder").recordingStatus },
+					},
 				},
-			},
-			inactive_sections = {
-				lualine_a = { "filename" },
-				lualine_b = {},
-				lualine_c = {},
-				lualine_x = {},
-				lualine_y = {},
-				lualine_z = { "location" },
-			},
-			tabline = {},
-			extensions = {},
-		})
-	end,
+				inactive_sections = {
+					lualine_a = { "filename" },
+					lualine_b = {},
+					lualine_c = {},
+					lualine_x = {},
+					lualine_y = {},
+					lualine_z = { "location" },
+				},
+				tabline = {},
+				extensions = {},
+			})
+		end,
+	},
+	{
+		"chrisgrieser/nvim-recorder",
+		dependencies = "rcarriga/nvim-notify", -- optional
+		opts = {}, -- required even with default settings, since it calls `setup()`
+	},
 }
