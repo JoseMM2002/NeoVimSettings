25b2bbb47300c9506d83bf8c364664c1c5fb733a
Jose Morales Medina <Jose.Morales3@thomsonreuters.com>
2025-11-03 10:10:12 -0600

    change tab behavior

--- lua/josemm/lazy/completion.lua
+++ lua/josemm/lazy/completion.lua
@@ -173,7 +173,6 @@ return {
 							mode = "symbol_text",
 						})(entry, vim.deepcopy(vim_item))
 						local highlights_info = require("colorful-menu").cmp_highlights(entry)
-
 						if highlights_info ~= nil then
 							vim_item.abbr_hl_group = highlights_info.highlights
 							vim_item.abbr = highlights_info.text
@@ -181,7 +180,6 @@ return {
 						local strings = vim.split(kind.kind, "%s", { trimempty = true })
 						vim_item.kind = " " .. (strings[1] or "") .. " "
 						vim_item.menu = ""
-
 						return vim_item
 					end,
 				},
@@ -211,11 +209,10 @@ return {
 						end
 					end),
 					["<Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						elseif luasnip.locally_jumpable(1) then
+						if luasnip.locally_jumpable(1) then
 							luasnip.jump(1)
 						else
+							cmp.abort()
 							fallback()
 						end
 					end, { "i", "s" }),
@@ -234,38 +231,6 @@ return {
 					expand = function(args)
 						luasnip.lsp_expand(args.body)
 					end,
-				},
-
-				sorting = {
-					priority_weight = 5,
-					comparators = {
-						function(entry1, entry2)
-							local kind1 = entry1.source.name
-							local kind2 = entry2.source.name
-							if kind1 == "buffer" and kind2 ~= "buffer" then
-								return false
-							elseif kind1 ~= "bufffer" and kind2 == "buffer" then
-								return true
-							end
-						end,
-						function(entry1, entry2)
-							local kind1 = entry1.source.name
-							local kind2 = entry2.source.name
-							if kind1 == "copilot" and kind2 ~= "copilot" then
-								return false
-							elseif kind1 ~= "copilot" and kind2 == "copilot" then
-								return true
-							end
-						end,
-						cmp.config.compare.order,
-						cmp.config.compare.offset,
-						cmp.config.compare.exact,
-						cmp.config.compare.score,
-						cmp.config.compare.recently_used,
-						cmp.config.compare.locality,
-						cmp.config.compare.kind,
-						cmp.config.compare.length,
-					},
 				},
 			})
 
