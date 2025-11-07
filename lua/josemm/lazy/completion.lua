ec8786de0eff9c93ef301af981cdf9ffc60599bd
Jose Morales Medina <jeolmmt@gmail.com>
2025-11-07 00:14:37 -0600

    use snacks picker

--- lua/josemm/lazy/completion.lua
+++ lua/josemm/lazy/completion.lua
@@ -9,6 +9,22 @@ return {
 		opts = {
 			integrations = {
 				nvim_cmp = true,
+				snacks = {
+					shelter = {
+						mask_on_copy = false, -- Whether to mask values when copying
+					},
+					keys = {
+						copy_value = "<C-y>", -- Copy variable value to clipboard
+						copy_name = "<C-u>", -- Copy variable name to clipboard
+						append_value = "<C-a>", -- Append value at cursor position
+						append_name = "<CR>", -- Append name at cursor position
+						edit_var = "<C-e>", -- Edit environment variable
+					},
+					layout = { -- Any Snacks layout configuration
+						preset = "dropdown",
+						preview = false,
+					},
+				},
 			},
 
 			shelter = {
@@ -24,10 +40,10 @@ return {
 				},
 				modules = {
 					cmp = true,
-					telescope_previewer = true,
-					telescope = true,
 					files = false,
 					peek = true,
+					snacks_previewer = true,
+					snacks = true,
 				},
 			},
 
---
+++ lua/josemm/lazy/picker.lua
@@ -0,0 +1,90 @@
+return {
+	{
+		"folke/snacks.nvim",
+		dependencies = {
+			{ "nvim-tree/nvim-web-devicons" },
+			{ "tpope/vim-fugitive" },
+		},
+		opts = {
+			gh = {
+				-- your gh configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			},
+			picker = {
+				matcher = { smart_case = true, fuzzy = true },
+				sources = {
+					gh_issue = {
+						-- your gh_issue picker configuration comes here
+						-- or leave it empty to use the default settings
+					},
+					gh_pr = {
+						-- your gh_pr picker configuration comes here
+						-- or leave it empty to use the default settings
+					},
+				},
+			},
+		},
+		config = function(_, opts)
+			local snacks = require("snacks")
+			snacks.setup(opts)
+			local P = snacks.picker
+
+			vim.keymap.set("n", "<leader>ff", function()
+				P.files({ hidden = true, ignored = true })
+			end, { desc = "Find files" })
+
+			vim.keymap.set("n", "<leader>fb", function()
+				P.buffers({ sort_lastused = true })
+			end, { desc = "Find buffers" })
+
+			vim.keymap.set("n", "<leader>fr", function()
+				P.grep({})
+			end, { desc = "Live grep" })
+
+			vim.keymap.set("n", "<leader>fh", function()
+				P.help({})
+			end, { desc = "Help" })
+
+			vim.keymap.set("n", "<leader>fs", function()
+				P.git_status({})
+			end, { desc = "Git status" })
+
+			vim.keymap.set("n", "<leader>fD", function()
+				P.diagnostics({})
+			end, { desc = "Diagnostics (workspace)" })
+
+			vim.keymap.set("n", "<leader>fd", function()
+				P.diagnostics_buffer({})
+			end, { desc = "Diagnostics (buffer)" })
+
+			vim.keymap.set("n", "<leader>fS", function()
+				P.lsp_symbols({})
+			end, { desc = "Symbols" })
+
+			vim.keymap.set("n", "gr", function()
+				P.lsp_references({})
+			end, { desc = "LSP refs" })
+
+			vim.keymap.set("n", "gd", function()
+				P.lsp_definitions({})
+			end, { desc = "LSP defs" })
+
+			vim.keymap.set("n", "<leader>tc", function()
+				P.colorschemes()
+			end, { desc = "Colorschemes" })
+
+			vim.keymap.set("n", "<leader>fn", function()
+				if P.registers then
+					P.registers()
+				end
+			end, { desc = "Registers" })
+
+			vim.keymap.set("n", "<leader>fu", function()
+				if P.undo then
+					P.undo()
+				end
+			end, { desc = "Undo history" })
+		end,
+	},
+}
