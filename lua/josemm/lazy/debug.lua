aa53e289f6a3c1ec429203719a62f21ec82bb9b0
Jose Morales Medina <Jose.Morales3@thomsonreuters.com>
2025-11-15 23:15:00 -0600

    add codelldb debugger

--- lua/josemm/lazy/debug.lua
+++ lua/josemm/lazy/debug.lua
@@ -61,6 +61,11 @@ return {
 				command = "gdb",
 				args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
 			},
+			lldb = {
+				type = "executable",
+				command = "lldb-dap",
+				name = "lldb",
+			},
 			["pwa-node"] = {
 				type = "server",
 				port = "${port}",
@@ -189,7 +194,7 @@ return {
 
 		dap.configurations.c = {
 			{
-				name = "Launch",
+				name = "Launch with gdb",
 				type = "gdb",
 				request = "launch",
 				program = function()
@@ -200,31 +205,20 @@ return {
 				args = get_args,
 			},
 			{
-				name = "Select and attach to process",
-				type = "gdb",
-				request = "attach",
-				program = function()
-					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-				end,
-				pid = function()
-					local name = vim.fn.input("Executable name (filter): ")
-					return require("dap.utils").pick_process({ filter = name })
-				end,
-				cwd = "${workspaceFolder}",
-			},
-			{
-				name = "Attach to gdbserver :1234",
-				type = "gdb",
-				request = "attach",
-				target = "localhost:1234",
+				name = "Launch with lldb",
+				type = "lldb",
+				request = "launch",
 				program = function()
 					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
 				end,
 				cwd = "${workspaceFolder}",
+				stopOnEntry = false,
+				args = get_args,
 			},
 		}
 		dap.configurations.cpp = dap.configurations.c
 		dap.configurations.rust = dap.configurations.c
+		dap.configurations.zig = dap.configurations.c
 		dap.configurations.go = {
 			{
 				type = "go",
