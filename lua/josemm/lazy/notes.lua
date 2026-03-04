65522c6bea96c93ebeade0fe379f110c8748bf98
Jose Morales Medina <Jose.Morales3@thomsonreuters.com>
2026-03-04 05:29:54 -0600

    disable notes log

--- lua/josemm/lazy/notes.lua
+++ lua/josemm/lazy/notes.lua
@@ -2,13 +2,12 @@ return {
 	"itsvinayak/nvim-notes.nvim",
 	dependencies = {
 		"nvim-telescope/telescope.nvim", -- Add Telescope as a dependency
-		"folke/which-key.nvim", -- Add WhichKey as a dependency (optional)
 	},
 	config = function()
 		require("notes").setup({
 			-- Optional configurations
 			path = "~/.notes", -- Custom path for notes (default is '~/.notes')
-			log_enabled = true, -- Enable logging (default is false)
+			log_enabled = false, -- Enable logging (default is false)
 			log_level = "INFO", -- Set log level to INFO
 			filetype = "md", -- Sets the notes filetype default is 'md'
 		})
