0aaf941311935ea4743acfc57641e18331dcd9d5
Jose Morales Medina <Jose.Morales3@thomsonreuters.com>
2026-03-08 03:51:37 -0600

    change diffview to codediff nvim

--- lua/josemm/lazy/gh.lua
+++ lua/josemm/lazy/gh.lua
@@ -1,22 +1,6 @@
 return {
 	{
-		"sindrets/diffview.nvim",
-		config = function()
-			require("diffview").setup()
-		end,
-	},
-	{
-		"ldelossa/gh.nvim",
-		dependencies = {
-			{
-				"ldelossa/litee.nvim",
-				config = function()
-					require("litee.lib").setup()
-				end,
-			},
-		},
-		config = function()
-			require("litee.gh").setup()
-		end,
+		"esmuellert/codediff.nvim",
+		cmd = "CodeDiff",
 	},
 }
